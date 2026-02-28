0654fc89790fecab822ab571856fee19df52f600
drrajivjesani99 <drrajivjesani9@gmail.com>
2026-02-28 19:47:13 +0530

    Update supabase.js
    New databse

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
-  
+const SUPABASE_URL = https://rwbhmfkqcezsfpqsxgun.supabase.co;
+const SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ3YmhtZmtxY2V6c2ZwcXN4Z3VuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyODcwMzUsImV4cCI6MjA4Nzg2MzAzNX0.YhvI5d9kwxbu7deDUWcNw0fNsXPHNGkwVLf3IvbzwAk;  
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
 
