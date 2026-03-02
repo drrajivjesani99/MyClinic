8e7d07acfa65152e3e06b07c8a6dff2bf29f10e5
drrajivjesani99 <drrajivjesani9@gmail.com>
2026-03-02 08:52:12 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { registerSW } from 'virtual:pwa-register';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { subscribeToPush } from './lib/notifications'
 
-registerSW({ immediate: true })
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
+
+// Ask for push permission after 3 seconds
+setTimeout(() => {
+  subscribeToPush('')
+}, 3000)
