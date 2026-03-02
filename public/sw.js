9a55a56068aed2bf68754787fbeddc791085b041
drrajivjesani99 <drrajivjesani9@gmail.com>
2026-03-02 08:44:56 +0530

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,29 @@
+self.addEventListener('push', function(event) {
+  const data = event.data ? event.data.json() : {}
+  const title = data.title || 'MyClinic'
+  const options = {
+    body: data.body || 'New update from MyClinic',
+    icon: '/icons/icon-192.png',
+    badge: '/icons/icon-192.png',
+    vibrate: [100, 50, 100],
+    data: { url: data.url || '/' }
+  }
+  event.waitUntil(
+    self.registration.showNotification(title, options)
+  )
+})
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close()
+  event.waitUntil(
+    clients.openWindow(event.notification.data.url || '/')
+  )
+})
+
+self.addEventListener('install', function(event) {
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(clients.claim())
+})
